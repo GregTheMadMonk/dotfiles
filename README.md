ba7594ad497cad3b25c68ebb3af7bb6438a987d6
GregTheMadMonk <yagreg7@gmail.com>
2019-11-04 03:14:04 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# dotfiles
+My dotfiles. Managed using GNU stow.
+
+Can be installed with `./deploy.sh`, or manually (in that case, look into **deploy.sh** anyway, you migh need to perform some additional actions).
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# dotfiles that must be copied to ~/
+echo Using stow to deploy dotfiles...
 stow -R -t ~ home_*
 
+echo Creating additional symlinks...
+echo "~/.tmux.conf -> ~/.tmux/.tmux.conf..."
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+echo "~/.tmux.conf.local -> ~/.tmux.conf.local..."
 ln -s ~/.tmux/.tmux.conf.local ~/.tmux.conf.local
