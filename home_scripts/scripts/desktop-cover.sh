db8f520f5d85e3b45a2bf80a053331cbed31546b
GregTheMadMonk <yagreg7@gmail.com>
2019-11-24 19:38:56 +0300

    Some fixes

--- home_scripts/scripts/desktop-cover.sh
+++ home_scripts/scripts/desktop-cover.sh
@@ -5,20 +5,20 @@ FLAG=1
 F=
 
 while [ true ]; do
-	DIR=$(dirname "$(cmus-remote -Q | grep "file " | sed "s/file\ //g")")
-	DIR=$((echo $DIR | grep -q "cue://") && (echo $DIR | sed "s/cue:\/\///g" | sed 's/\/[^/]*$//g') || (echo $DIR))
-	echo $DIR
+	DIR="$(dirname "$(cmus-remote -Q | grep "file " | sed "s/file\ //g")")"
+	DIR="$((echo "$DIR" | grep -q "cue://") && (echo "$DIR" | sed "s/cue:\/\///g" | sed 's/\/[^/]*$//g') || (echo "$DIR"))"
+	echo "$DIR"
 	if [ "$DIR" != "." ]; then
-		COVER=$(ls "$DIR" | grep -i cover | grep -i .jpg)
+		COVER=$(ls "$DIR"/*.jpg | grep -i cover)
 		if [ $? -ne 0 ]; then
-			COVER=$(ls "$DIR" | grep -i front | grep -i .jpg)
+			COVER=$(ls "$DIR"/*.jpg | grep -i front)
 			if [ $? -ne 0 ]; then
-				COVER=$(ls "$DIR" | grep -m1 jpg)
+				COVER=$(ls "$DIR"/*.jpg | grep -m1 .jpg)
 			fi
 		fi
 		if [ $? -eq 0 ]; then
 			if [ "$F" != "$DIR/$COVER" ]; then
-				F=$DIR/$COVER
+				F=$COVER
 				echo  Updating with $F
 				magick -gravity Center $BACKGROUND \( "$F" -resize 490x490 \) $MASK -composite +antialias ~/Pictures/.tmp-desktop.png
 				hsetroot -fill ~/Pictures/.tmp-desktop.png
