450d0cc76c1364352f0a18ede2ff4b5feaff0a64
GregTheMadMonk <yagreg7@gmail.com>
2020-04-15 01:57:11 +0300

    Further improvements

--- home_scripts/scripts/exit_session.sh
+++ home_scripts/scripts/exit_session.sh
@@ -5,7 +5,7 @@ REBOOT="2. Reboot"
 SHUTDOWN="3. Shut down"
 CANCEL="4. Cancel"
 
-ROFI='rofi -dmenu -separator-style none -font "mono 11" -bw 0 -disable-history -hide-scrollbar -color-window "#222222, #222222, #b1b4b3" -color-normal "#222222, #b1b4b3, #222222, #005577, #b1b4b3" -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3"'
+ROFI='rofi -dmenu -separator-style none -bw 0 -disable-history -hide-scrollbar'
 
 RESULT=$(echo -e "$END_SESSION\n$REBOOT\n$SHUTDOWN\n$CANCEL" | sh -c "$ROFI -mesg 'Pick an action'")
 
