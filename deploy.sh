e34a668b84ea298c1bac99678782b45f76457382
GregTheMadMonk <yagreg7@gmail.com>
2020-04-14 08:35:18 +0300

    Major theme change

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 
-THEME="$1"
-if [ "$THEME" = "" ]; then
-	echo No theme specified
-	exit 1;
-fi
-
-echo Using theme $THEME
 echo Using stow to deploy not theme-specific dotfiles...
 echo "~/ subfolders"
 stow -R -t ~ home_* --override=* --verbose=0
 echo "/usr subfolders (requires sudo)"
 sudo stow -R -t /usr usr_* --override=* --verbose=0
-
-echo Using stow to deploy theme-specific dotfiles...
-echo "~/ subfolders"
-stow -R -t ~ "$THEME"_home_* --override=* --verbose=0
-echo "/usr subfolders (requres sudo)"
-sudo stow -R -t /usr "$THEME"_usr_* --override=* --verbose=0
 
 echo Creating additional symlinks...
 echo "~/.tmux.conf -> ~/.tmux/.tmux.conf..."
