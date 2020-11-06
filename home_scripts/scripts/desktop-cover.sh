7caa32258281e940965d1213eb5e790e8bb816a0
GregTheMadMonk <yagreg7@gmail.com>
2020-11-06 21:41:48 +0300

    Use album-art instead of if-else-if hell

--- home_scripts/scripts/desktop-cover.sh
+++ home_scripts/scripts/desktop-cover.sh
@@ -5,23 +5,18 @@ FLAG=1
 F=
 
 while [ true ]; do
-	FILE="$(cmus-remote -Q | grep "file " | sed "s/file\ //g")"
+	ROUT="$(cmus-remote -Q)"
+	FILE="$(grep "file " <<< "$ROUT" | sed "s/file\ //g")"
 	DIR="$(dirname "$FILE")"
 	DIR="$((echo "$DIR" | grep -q "cue://") && (echo "$DIR" | sed "s/cue:\/\///g" | sed 's/\/[^/]*$//g') || (echo "$DIR"))"
 	echo "$DIR"
 	if [ "$DIR" != "." ]; then
-		COVER="$FILE.temp.jpg"
+		COVER="/home/$USER/Pictures/.cover.temp.jpg"
 		rm "$COVER"
-		ffmpeg -i "$FILE" "$COVER"
-		if [ $? -ne 0 ]; then
-			COVER=$(ls -S "$DIR"/*.jpg | grep -i cover)
-			if [ $? -ne 0 ]; then
-				COVER=$(ls -S "$DIR"/*.jpg | grep -i front)
-				if [ $? -ne 0 ]; then
-					COVER=$(ls -S "$DIR"/*.jpg | grep -m1 .jpg)
-				fi
-			fi
-		fi
+		ARTIST="$(grep "tag artist " <<< "$ROUT" | sed "s/tag artist //g")"
+		ALBUM="$(grep "tag album " <<< "$ROUT" | sed "s/tag album //g")"
+		echo $ARTIST - $ALBUM
+		curl "$(album-art "$ARTIST" "$ALBUM")" -o "$COVER"
 		if [ $? -eq 0 ]; then
 			if [ "$F" != "$DIR/$COVER" ]; then
 				F="$COVER"
