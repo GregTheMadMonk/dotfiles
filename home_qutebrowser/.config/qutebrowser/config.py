ec2326bbbe767440b9177915dc78446f7c9aeb0e
GregTheMadMonk <yagreg7@gmail.com>
2021-10-25 10:07:27 +0300

    Update dotfiles

---
+++ home_qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.source("base16-qutebrowser/themes/default/base16-material-palenight.config.py")
+config.source("qutenyan/nyan.py")
+
+# c.content.user_stylesheets = "solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css";
+c.content.user_stylesheets = "font.css";
+
+config.load_autoconfig()
