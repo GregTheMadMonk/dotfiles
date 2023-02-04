4449517247952a81fbcf6295e967fe9ec360aa30
GregTheMadMonk <yagreg7@gmail.com>
2023-02-04 18:52:31 +0300

    I don't use this anymore

--- home_qutebrowser/.config/qutebrowser/config.py
+++
@@ -1,7 +0,0 @@
-config.source("base16-qutebrowser/themes/default/base16-material-palenight.config.py")
-config.source("qutenyan/nyan.py")
-
-# c.content.user_stylesheets = "solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css";
-c.content.user_stylesheets = "font.css";
-
-config.load_autoconfig()
